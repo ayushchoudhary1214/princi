d76d8a9511b741dbfb3d56984953860a756f5ab5
ayushchoudhary1214 <ec2-user@ip-172-31-83-202.ec2.internal>
2020-01-07 07:03:42 +0000

    added new file

--- richa.java
+++ richa.java
@@ -4,6 +4,6 @@ class richa
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, richa"); 
+        System.out.println("Hello, richa jain "); 
     } 
 } 
