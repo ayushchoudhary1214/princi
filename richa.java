87ab4973bb14686795d92c60ea100166d9ded49d
ayushchoudhary1214 <ayushchoudhary26@gmail.com>
2020-01-07 10:04:16 +0000

    new commit

--- richa.java
+++ richa.java
@@ -4,6 +4,6 @@ class richa
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello s3"); 
+        System.out.println("Hello world"); 
     } 
 } 
